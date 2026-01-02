8e9251384fc76cdcc291f3417b19ebc275759209
Siddhant Rathi <sid20rathi@gmail.com>
2026-01-02 15:45:39 +0530

    made changes in the hero section

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -8,6 +8,7 @@ import GridLayout from "@/components/GridLyout";
 import { WordRotate } from "@/components/ui/word-rotate";
 import { useState } from "react";
 import { IconCircleHalf2 } from "@tabler/icons-react";
+import { FloatingDockPage } from "@/components/floatnavbar";
 
 export default function Home() {
 
@@ -21,21 +22,23 @@ export default function Home() {
   }
   return (
     <div className=" flex  justify-center items-center flex flex-col">
+    
       <div className="h-[100px] w-screen border-b-2 border-dashed  ">
         <div className="h-[100px] w-[60%] m-auto border-x-2 border-dashed">
           <Navbarpage/>
+          <FloatingDockPage/>
           
 
         </div>
 
       </div>
       <div className="h-full w-[60%] m-auto border-x-2 border-dashed">
-      <div className=" pl-10 flex flex-row justify-center  ">
+      <div className=" pl-10 flex flex-row justify-between md:justify-center lg:justify-center xl:justify-center">
         <div>
-          <div className="flex flex-col pt-28 "><span className="inline-block text-4xl font-bold  sm:text-5xl xl:text-6xl/none">Hi, I'm Siddhant</span>
-        <div>
+          <div className="flex flex-col pt-28 "><span className="inline-block text-3xl font-bold  sm:text-4xl md:text-5xl xl:text-6xl/none">Hi, I'm Siddhant</span>
+        <div className="pb-10 ">
           <WordRotate
-      className="text-2xl pl-10 font-mono tracking-tighter text-neutral-500 dark:text-white hover:text-blue-400 dark:hover:text-blue-400"
+      className=" text-sm md:text-xl xl:text-2xl fixed font-mono tracking-tighter text-neutral-500 dark:text-white hover:text-blue-400 dark:hover:text-blue-400"
       words={["- AI Engineer", "- Full Stack web developer",]}
     />
         </div>
@@ -44,15 +47,15 @@ export default function Home() {
         </div>
 
         </div>
-        <div className="pl-14 pt-24 ">
+        <div className="pt-28 pr-2 md:pl-7 md:pb-5 lg">
           <button onClick={visibleProf}>
-            {visible ? <div className="flex flex-row items-end">
+            {visible ? <div className="flex flex-row items-center">
               <img
             src="/siddhant.png"
             alt="profile"
             width={120}
             height={120}
-            className="rounded-full zoom-in-25 cursor-pointer z-20 shadow-2xl "
+            className="rounded-full hidden md:block size-16 md:size-20 lg:size-28 xl:size-28 cursor-pointer z-20 shadow-2xl "
           />
           
             </div>:<img
@@ -60,7 +63,7 @@ export default function Home() {
             alt="profile"
             width={120}
             height={120}
-            className="rounded-full zoom-in-25 cursor-pointer z-20 shadow-2xl "
+            className="rounded-full hidden md:block size-16 md:size-20 lg:size-28 xl:size-28 cursor-pointer z-20 shadow-2xl "
           />}
           </button>
           
---
+++ website/components/floatnavbar.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { FloatingDock } from "@/components/ui/floating-dock";
+import {
+  IconBrandGithub,
+  IconBrandLinkedin,
+  IconBrandX,
+  IconExchange,
+  IconHome,
+  IconNewSection,
+  IconTerminal2,
+} from "@tabler/icons-react";
+
+export function FloatingDockPage() {
+  const links = [
+    {
+      title: "Home",
+      icon: (
+        <IconHome className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+
+    {
+      title: "Twitter",
+      icon: (
+        <IconBrandX className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "GitHub",
+      icon: (
+        <IconBrandGithub className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+    {
+      title: "LinkedIn",
+      icon: (
+        <IconBrandLinkedin
+ className="h-full w-full text-neutral-500 dark:text-neutral-300" />
+      ),
+      href: "#",
+    },
+  ];
+  return (
+    <div className="flex items-center justify-center h-[35rem] w-full ">
+      <FloatingDock
+        mobileClassName="translate-y-20" // only for demo, remove for production
+        items={links}
+      />
+    </div>
+  );
+}
