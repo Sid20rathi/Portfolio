3b5fec962f61550a3247760ce9e8b7bc9e57ffa8
Siddhant Rathi <sid20rathi@gmail.com>
2026-01-07 15:20:53 +0530

    added activity section

--- website/components/page/Activity.tsx
+++ website/components/page/Activity.tsx
@@ -1,41 +1,44 @@
+"use client";
 
-import {GitHubCalendar} from 'react-github-calendar';
+import { GitHubCalendar, Activity } from "react-github-calendar";
 
-export default function Activity() {
+export default function ActivityComponent() {
+    const selectLastTwoMonths = (data: Activity[]): Activity[] => {
+        const twoMonthsAgo = new Date();
+        twoMonthsAgo.setMonth(twoMonthsAgo.getMonth() - 2);
 
-    const selectLastHalfYear = (contributions:any) => {
-  const currentYear = new Date().getFullYear();
-  const currentMonth = new Date().getMonth();
-  const shownMonths = 6;
-
-  return contributions.filter((activity:any) => {
-    const date = new Date(activity.date);
-    const monthOfDay = date.getMonth();
-
-    return (
-      date.getFullYear() === currentYear &&
-      monthOfDay > currentMonth - shownMonths &&
-      monthOfDay <= currentMonth
-    );
-  });
-};
-    
- 
-
-
-
+        return data.filter((activity) => {
+            return new Date(activity.date) >= twoMonthsAgo;
+        });
+    };
 
     return (
-        <div>
+        <div >
+            <div className="border-x-2 border-dashed border-b-2 font-serif text-lg p-3 uppercase tracking-[0.01em]">
+                My Activity
 
-        <GitHubCalendar 
-  username="sid20rathi" 
-  transformData={selectLastHalfYear} 
-  labels={{
-    totalCount: '{{count}} contributions in the last half year',
-  }}
-   
+            </div>
+            <div className="p-3 border-x-2 border-dashed border-b-2 ">
+                <div className="grid grid-cols-2 overflow-hidden">
+                    <div className="bg-red-500">
+            <GitHubCalendar
+            username="sid20rathi"
+            transformData={selectLastTwoMonths}
+            labels={{
+              totalCount: "{{count}} Github Contributions in the last 2 months",
+            }}
+            colorScheme='light'
+            maxLevel={0}
+           
+         
             
+            
+            />
         </div>
-    )
+
+                </div>
+            </div>
+
+        </div>
+    );
 }
